2b3d43b10066d0db3fa8fe2fbf55b7929d697b5d
amitkhatri4 <khatriamit706@gmail.com>
2025-03-18 22:37:39 +0600

    py file

---
+++ wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for NepalRental project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NepalRental.settings')
+
+application = get_wsgi_application()
