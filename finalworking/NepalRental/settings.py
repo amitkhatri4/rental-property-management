d4f6cf4314e7b121015cd178c1ae7426aa0e21ef
amitkhatri4 <khatriamit706@gmail.com>
2025-04-16 21:06:51 +0600

    Email Verification

--- finalworking/NepalRental/settings.py
+++ finalworking/NepalRental/settings.py
@@ -39,8 +39,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'sandipkhadka9810@gmail.com'
-EMAIL_HOST_PASSWORD = 'gbsu euua unxh thri'
+EMAIL_HOST_USER = 'khatriamit231@gmail.com'
+EMAIL_HOST_PASSWORD = 'rwno uyug vogp kfwh'
 
 
 
